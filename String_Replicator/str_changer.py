47047e4027538d7637018d006021ebffd46bd65d
Scepticae <samuel_leonard@comcast.net>
2019-09-11 15:49:22 -0400

    Program to change second string to first

---
+++ String_Replicator/str_changer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+# Get the two strings
+def edit_strs():
+  str1 = sys.argv[1]
+  str2 = sys.argv[2]
+  counter = 0
+
+  # Edit strings
+
+def str_editer(sA, sB):
+  sC = sB.split()
+
+  for index in range(len(sA)):
+
