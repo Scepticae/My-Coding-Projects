0bbf05ef3be43be76061200ef019ec9802cf2dd2
Scepticae <53708737+Scepticae@users.noreply.github.com>
2020-01-25 21:03:11 -0500

    Start Python Program to reverse interleave array

---
+++ My Python Coding/My Daily Coding/reverseInterleave.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Program Description:
+# This program takes an array and reverse interleaves the second half
+# of the array with the first half.
+
+def reverseInterleave(n_array):
+  for n in range(len(n_array) / 2):
